25dfb1f57e25808557a70c743f944da34c3918ea
bethek <kreshnikiberdemaj@gmail.com>
2017-05-30 10:53:50 +0200

    heroku deploy

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="index.php" method="post" enctype="multipart/form-data">
+            <input type="file" name"yourimage">
+            <input type="hidden" name="MAX_FILE_SIZE" value="100000"> <!--100Ko max-->
+            <input type="submit" name="envoyer" value="Envoyer le fichier"
+
+            </table>
+        </form>
+    </body>
+</html>
---
+++ upload.php
@@ -0,0 +1,57 @@
+
+<?php
+/*
+    NOM VARIABLE
+ Le nom 	$_FILES['avatar']['name']
+ Le chemin du fichier temporaire 	$_FILES['avatar']['tmp_name']
+ La taille (peu fiable, dépend du navigateur) 	$_FILES['avatar']['size']
+ Le type MIME (peu fiable, dépend du navigateur) 	$_FILES['avatar']['type']
+ Un code d'erreur si besoin 	$_FILES['avatar']['error']
+*/
+
+
+if(isset ($_FILES['yourimage']))
+ {
+    # code..
+    $dossier = 'upload/';
+    $fichier = basename ($_FILES['yourimage'] ['name']);
+    if(move_uploaded_file($_FILES['yourimage'] ['tmp_name'], $dossier . $fichier)) // si fonction renvoie TRUE alors OK
+
+    {
+        echo 'upload effectué avec succès !';
+    }
+    else //sinon (renvoie la fonction FALSE)
+    {
+        echo 'Echec de l\'upload!';
+    }
+
+}
+
+
+
+/*TABLEAU EXTENSION AUTORISEES*/
+
+$extensions =array('.jpg','.png','.jpeg','.gif','.WebP')
+
+$extension = strrrch($_FILES['yourimage']['name'], '.');
+//Ensuite on teste
+if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+{
+    $erreur = 'ONLY FILETYPE : .jpg, .jpeg, .png, .gif, .Webp'
+}
+
+
+
+
+    /*TAILLE MAXI DU FICHIER*/
+
+// taille maximum (en octets)
+$taille_maxi = 100000;
+//Taille du fichier
+$taille = filesize($_FILES['avatar']['tmp_name']);
+if($taille>$taille_maxi)
+{
+     $erreur = 'Le fichier est trop gros...';
+}
+
+ ?>
